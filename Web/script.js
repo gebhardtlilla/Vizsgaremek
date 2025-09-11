c0cae100e90bf3c844027e93bdf0570f47552661
Gebhardt Lilla <gebhardt.lilla@ckik.loc>
2025-09-11 13:56:21 +0200

    weboldal_otleteles

---
+++ Web/script.js
@@ -0,0 +1,5 @@
+// Példa: ha a "Kezdés" gombra kattint, átnavigálhat az ajándékok oldalra
+document.getElementById("startBtn").addEventListener("click", function() {
+  alert("Kezdődik az ajándék keresés!");
+  // window.location.href = "/ajandekok.html"; // majd később erre lehet irányítani
+});
